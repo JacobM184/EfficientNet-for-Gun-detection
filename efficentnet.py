bfe138c60da90e550d97401644b609c7697ab834
JacobM184 <55066056+JacobM184@users.noreply.github.com>
2020-04-13 17:48:05 +1200

    Update efficientnet.py
    - Changed code left over from testing on other datasets
    - Fixed some indentation and formatting errors
    - added comments for Dataset loading section

--- efficentnet.py
+++ efficentnet.py
@@ -21,23 +21,32 @@ image_augmentations = {
         transforms.ToTensor(),
         #       transforms.RandomErasing(),
         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
-        ]),
+    ]),
 
     'valid': transforms.Compose([
         transforms.Resize(size=256),
         transforms.CenterCrop(size=224),
         transforms.ToTensor(),
         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-       ])}
+    ]),
+}
 ###########################################################################################################################
         
 ############################ Dataset loading section ######################################################################
-dataDirectory = "hymenoptera_data"  # will change dir later
+dataDirectory = "gunData"  # will change dir later
+
+#defines training and validation sets
 gunDataset = {i: datasets.ImageFolder(os.path.join(dataDirectory, i), image_augmentations[i]) for i in ['train', 'valid']}
 
+#loads defined training and validation sets
 dataLoaders = {i: DataLoader(gunDataset[i], batch_size=4, shuffle=True) for i in ['train', 'valid']}
 
+#gets class names
 classNames = gunDataset['train'].classes
+
+#gets size of training set and validation set
 datasetSize = {i: len(gunDataset[i]) for i in ['train', 'valid']}
+
+#chooses whether to use GPU or CPU depending on availability
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 ###########################################################################################################################
