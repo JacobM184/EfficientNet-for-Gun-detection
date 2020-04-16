ec14a19ccba05e5da521968f5b1f619985050f68
Oh-hi-marx <40456769+Oh-hi-marx@users.noreply.github.com>
2020-04-16 18:33:34 +1200

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,60 +1,35 @@
 class MBConv(nn.Module):
-    def __init__(self, inputCh, outputCh, filterSize, stride, expandRatio, SERatio, DropPRate):
-        super(MBConv, self).__init__()
+        def __init__(self, inputCh, outputCh, filterSize, stride, expandRatio, SERatio, DropPRate):
+            super(MBConv, self).__init__()
+            self.plc = ((stride == 1) and (inputCh == outputCh))
+            expandedCh = inputCh * expandRatio
+            MBconv = []
 
-        expandedCh = inputCh * expandRatio
-        MBconv = []
+            if (expandRatio != 1):
+                expansionPhase = nn.Sequential(
+                    nn.Conv2d(inputCh, expandedCh, kernel_size=1, bias=False), nn.BatchNorm2d(expandedCh),
+                    Swish()
+                )
+                MBconv.append(expansionPhase)
 
-        if (expandRatio != 1):
-            expansionPhase = nn.Sequential(
-                nn.Conv2d(inputCh, expandedCh, kernel_size=1, bias=False), nn.BatchNorm2d(outputCh),
-                swish()
+            DepthwisePhase = nn.Sequential(
+                nn.Conv2d(expandedCh, expandedCh, filterSize, stride, filterSize // 2, groups=expandedCh, bias=False),
+                nn.BatchNorm2d(expandedCh), Swish()
             )
-            MBconv.append(expansionPhase)
+            MBconv.append(DepthwisePhase)
 
-        DepthwisePhase = nn.Sequential(
-            nn.Conv2d(expandedCh, expandedCh, filterSize, stride, filterSize // 2, groups=expandedCh, bias=False),
-            nn.BatchNorm2d(outputCh), swish()
-        )
-        MBconv.append(DepthwisePhase)
+            # insert SqueezeAndExcite here later
 
-        # insert SqueezeAndExcite here later
+            projectionPhase = nn.Sequential(
+                nn.Conv2d(expandedCh, outputCh, kernel_size=1, bias=False), nn.BatchNorm2d(outputCh)
+            )
+            MBconv.append(projectionPhase)
 
-        projectionPhase = nn.Sequential(
-            nn.Conv2d(expandedCh, outputCh, filter_size=1, bias=False), nn.BatchNorm2d(outputCh)
-        )
-        MBconv.append(projectionPhase)
-
-        self.MBConvLayers = nn.Sequential(MBconv)
+            self.MBConvLayers = nn.Sequential(*MBconv)
 
 
-    def forward(self, x):
-        if ((stride == 1) and (inputCh == outputCh)):
-            return  ######## Dropout stuff
-        else:
-            return self.MBConvLayers(x)
-        
-        
-        
-        
-########### Squeeze and Excitation block ###################
-class SqueezeAndExcitation(nn.Module):
-    def __init__(self, inputCh, squeezeCh, SERatio):
-        super(SqueezeAndExcitation, self).__init__()
-
-        squeezeChannels = int(squeezeCh * SERatio)
-
-        # May have to use AdaptiveAvgPool3d instead, but
-        # we need to try this out first in case
-        self.GAPooling = nn.AdaptiveAvgPool2d((1, 1))
-        self.Linear1(inputCh, squeezeChannels)
-        self.nonLinearAct1 = nn.ReLU()  # may change to Swish
-        self.Linear2(squeezeChannels, inputCh)
-        self.nonLinearAct2 = nn.Sigmoid()
-
-    def forward(self, x):
-        y = self.GAPooling(x)
-        y = self.nonLinearAct1(self.Linear1(y))
-        y = self.nonLinearAct2(self.Linear2(y))
-        y = x * y
-        return y
+        def forward(self, x):
+            if self.plc:
+                return  ######## Dropout stuff
+            else:
+                return self.MBConvLayers(x)
